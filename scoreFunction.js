a5b7a4a6b13159c03a9af6428077a80bc8c8830e
Karan Desai <kdesai@KDESAI-AIR.local>
2020-07-27 22:19:48 -0400

    score

--- scoreFunction.js
+++ scoreFunction.js
@@ -38,20 +38,45 @@ function getScore(mentorObj, studentObj) {
   }
 
   var hobMatchScore = 0;
-  if (mentorObj.hobby1 === studentObj.hobby1 || mentorObj.hobby1 === studentObj.hobby2 || mentorObj.hobby1 === studentObj.hobby3){
+  var mentorHob2 = "";
+  var studentHob2 = "";
+  var mentorHob3 = "";
+  var studentHob3 = "";
+
+  if (mentorObj.hobby2 === "Other") {
+    mentorHob2 = mentorObj.hobby2other;
+  }
+  else {
+    mentorHob2 = mentorObj.hobby2;
+  }
+  if (studentObj.hobby2 === "Other") {
+    studentHob2 = studentObj.hobby2other;
+  }
+  else {
+    studentHob2 = studentObj.hobby2;
+  }
+
+  if (mentorObj.hobby3 === "Other") {
+    mentorHob3 = mentorObj.hobby3other;
+  }
+  else {
+    mentorHob3 = mentorObj.hobby3;
+  }
+  if (studentObj.hobby3 === "Other") {
+    studentHob3 = studentObj.hobby3other;
+  }
+  else {
+    studentHob3 = studentObj.hobby3;
+  }
+
+  if (mentorObj.hobby1 === studentObj.hobby1 || mentorObj.hobby1 === studentHob2 || mentorObj.hobby1 === studentHob3){
     hobMatchScore += 1;
   }
-  if (mentorObj.hobby2 === studentObj.hobby1 || mentorObj.hobby2 === studentObj.hobby2 || mentorObj.hobby2 === studentObj.hobby3){
+  if (mentorHob2 === studentObj.hobby1 || mentorHob2 === studentHob2 || mentorHob2 === studentHob3){
     hobMatchScore += 1;
   }
-  if (mentorObj.hobby3 === studentObj.hobby1 || mentorObj.hobby3 === studentObj.hobby2 || mentorObj.hobby3 === studentObj.hobby3){
+  if (mentorHob3 === studentObj.hobby1 || mentorHob3 === studentHob2 || mentorHob3 === studentHob3){
     hobMatchScore += 1;
-  }
-  if (mentorObj.hobby2 === "Other" && ((mentorObj.hobby2 === studentObj.hobby2) | (mentorObj.hobby2 === studentObj.hobby3))) {
-    hobMatchScore -= 1;
-  }
-  if (mentorObj.hobby3 === "Other" && ((mentorObj.hobby3 === studentObj.hobby2) | (mentorObj.hobby3 === studentObj.hobby3))) {
-    hobMatchScore -= 1;
   }
 
 
@@ -65,65 +90,169 @@ function getScore(mentorObj, studentObj) {
     hobScore += 1.5;
   }
 
+  var mentorMaj = "";
+  var studentMaj = "";
 
-  if (mentorObj.Major === studentObj.Major){
-    majScore += 1;
+  if (mentorObj.Major === "Other") {
+    mentorMaj = mentorObj.MajorOther;
   }
-  if (mentorObj.Major === studentObj.Minor || mentorObj.Minor === studentObj.Major){
+  else {
+    mentorMaj = mentorObj.Major;
+  }
+  if (studentObj.Major === "Other") {
+    studentMaj = studentObj.MajorOther;
+  }
+  else {
+    studentMaj = studentObj.Major;
+  }
+
+  var mentorMin = "";
+  var studentMin = "";
+
+  if (mentorObj.Minor === "Other") {
+    mentorMin = mentorObj.MinorOther;
+  }
+  else {
+    mentorMin = mentorObj.Minor;
+  }
+  if (studentObj.Minor === "Other") {
+    studentMin = studentObj.MinorOther;
+  }
+  else {
+    studentMin = studentObj.Minor;
+  }
+
+
+  if (mentorMaj === studentMaj){
+    if (mentorMin === "n/a" || studentMin === "n/a") {
+      majScore += 1.25;
+    }
+    else {
+      majScore += 1;
+    }
+  }
+
+  if (mentorMaj === studentMin || mentorMin === studentMaj){
     majScore += 0.5;
   }
-  if (mentorObj.Minor === studentObj.Minor){
-    majScore += 0.5;
+
+  if (mentorMin === studentMin){
+    if (mentorMin === "n/a" || studentMin === "n/a") {
+      majScore += 0;
+    }
+    else {
+      majScore += 0.5;
+    }
+  }
+
+
+  var mentorRes2 = "";
+  var studentRes2 = "";
+
+  if (mentorObj.research2 === "Other") {
+    mentorRes2 = mentorObj.research2Other;
+  }
+  else if (mentorObj.research2 === "none") {
+    mentorRes2 = 'na';
+  }
+  else {
+    mentorRes2 = mentorObj.research2;
+  }
+
+  if (studentObj.research2 === "Other") {
+    studentRes2 = studentObj.research2Other;
+  }
+  else if (studentObj.research2 === "none") {
+    studentRes2 = 'na';
+  }
+  else {
+    studentRes2 = studentObj.research2;
   }
 
   var researchMatchScore = 0;
-  if (mentorObj.research1 === studentObj.research1 || mentorObj.research1 === studentObj.research2){
+  if (mentorObj.research1 === studentObj.research1 || mentorObj.research1 === studentRes2){
     researchMatchScore += 1;
   }
-  if (mentorObj.research2 === studentObj.research1 || mentorObj.research2 === studentObj.research2){
-    researchMatchScore += 1;
-  }
-  if (mentorObj.research1 === "N/A" && ((mentorObj.research1 === studentObj.research1) | (mentorObj.research1 === studentObj.research2))) {
-    researchMatchScore -= 1;
-  }
-  if (mentorObj.research2 === "N/A" && ((mentorObj.research2 === studentObj.research1) | (mentorObj.research2 === studentObj.research2))) {
-    researchMatchScore -= 1;
+  if (mentorRes2  === studentObj.research1 || mentorRes2  === studentRes2){
+    if (mentorRes2 === 'na' && studentRes2 === 'na') {
+      researchMatchScore += 0;
+    }
+    else {
+      researchMatchScore += 1;
+    }
   }
 
 
   if (researchMatchScore === 1){
-    reScore = 1;
+    if (mentorRes2 === 'na' || studentRes2 === 'na') {
+      reScore = 1.25;
+    }
+    else {
+      reScore = 1;
+    }
   }
 
   if (researchMatchScore === 2){
     reScore = 1.5;
   }
 
-  var volMatchScore = 0;
-  if (mentorObj.volunteer1 === studentObj.volunteer1 || mentorObj.volunteer1 === studentObj.volunteer2){
-    volMatchScore += 1;
+  var mentorVol2 = "";
+  var studentVol2 = "";
+  if (mentorObj.volunteer2 === "Other") {
+    mentorVol2 = mentorObj.volunteer2Other;
   }
-  if (mentorObj.volunteer2 === studentObj.volunteer1 || mentorObj.volunteer2 === studentObj.volunteer2){
-    volMatchScore += 1;
+  else if (mentorObj.volunteer2 === "none") {
+    mentorVol2 = 'na';
   }
-  if (mentorObj.volunteer1 === "N/A" && ((mentorObj.volunteer1 === studentObj.volunteer1) | (mentorObj.volunteer1 === studentObj.volunteer2))) {
-    volMatchScore -= 1;
-  }
-  if (mentorObj.volunteer2 === "N/A" && ((mentorObj.volunteer2 === studentObj.volunteer1) | (mentorObj.volunteer2 === studentObj.volunteer2))) {
-    volMatchScore -= 1;
+  else {
+    mentorVol2 = mentorObj.volunteer2;
   }
 
+  if (studentObj.volunteer2 === "Other") {
+    studentVol2 = studentObj.volunteer2Other;
+  }
+  else if (studentObj.volunteer2 === "none") {
+    studentVol2 = 'na';
+  }
+  else {
+    studentVol2 = studentObj.volunteer2;
+  }
+
+  var volMatchScore = 0;
+  if (mentorObj.volunteer1 === studentObj.volunteer1 || mentorObj.volunteer1 === studentVol2){
+    volMatchScore += 1;
+  }
+  if (mentorVol2 === studentObj.volunteer1 || mentorVol2 === studentVol2){
+    if (mentorVol2 === 'na' && studentVol2 === 'na') {
+      volMatchScore += 0;
+    }
+    else {
+      volMatchScore += 1;
+    }
+  }
+
+
+
   if (volMatchScore === 1){
-    volScore = 0.75;
+    if (mentorVol2 === 'na' || studentVol2 === 'na') {
+      volScore= 1;
+    }
+    else {
+      reScore = 0.75;
+    }
   }
 
   if (volMatchScore === 2){
     volScore = 1.25;
   }
 
+
+
+
   if(mentorObj.Undergrad === studentObj.Undergrad){
     undgScore = 1.5;
   }
+
 
   var profMatchScore = 0;
   if (mentorObj.profession1 === studentObj.profession1 || mentorObj.profession1 === studentObj.profession2){
@@ -135,6 +264,9 @@ function getScore(mentorObj, studentObj) {
   if (mentorObj.profession2 === "Other" && studentObj.profession2 === "Other") {
     profMatchScore -= 1;
   }
+  if (mentorObj.profession2 === "none" && studentObj.profession2 === "none") {
+    profMatchScore -= 1;
+  }
 
   if (profMatchScore === 1){
     profScore = 0.5;
@@ -143,6 +275,7 @@ function getScore(mentorObj, studentObj) {
   if (profMatchScore === 2){
     profScore = 1;
   }
+
 
   var gapMatchScore = 0;
   if (mentorObj.gap1 === studentObj.gap1 || mentorObj.gap1 === studentObj.gap2){
@@ -158,7 +291,17 @@ function getScore(mentorObj, studentObj) {
       gapScore = 1.5;
     }
     else {
-      gapMatchScore += 1;
+      if (mentorObj.gap2 === "Other") {
+        if (mentorObj.gap2Other === studentObj.gap2Other) {
+          gapMatchScore += 1;
+        }
+        else {
+          gapMatchScore += 0;
+        }
+      }
+      else {
+        gapMatchScore += 1;
+      }
     }
   }
 
@@ -169,6 +312,7 @@ function getScore(mentorObj, studentObj) {
   if (gapMatchScore === 2){
     gapScore = 1.5;
   }
+
 
   var fmult = 2.75;
   var smult = 2;
